c7a52ee80da4a17e88674272685f75c3fdefc75a
Ian Freitas <ianfreitas1@poli.ufrj.br>
2022-01-16 22:16:46 -0300

    Removes unused code

--- DatabaseSystem.Hash/Bucket.h
+++ DatabaseSystem.Hash/Bucket.h
@@ -2,7 +2,6 @@
 #include "../DatabaseSystem.Core/Record.h"
 #include "../DatabaseSystem.Core/Block.h"
 
-#define MAX_RECORDS 9
 
 class Bucket
 {
@@ -10,6 +9,5 @@ public:
 	Bucket();
 	~Bucket();
 	unsigned int hash;
-	//unsigned int recordsCount;
 	unsigned long long blockNumber;
 };
